b057a79a3d0422ef11127223a2bfb42916fe1e7d
Danila Voronkov <danilavoronkov2002@gmail.com>
2022-11-03 15:31:25 +0300

    feat(seo): Added seo optimization

---
+++ next-sitemap.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: 'https://movie-tracker.dapzer.ru/',
+  generateRobotsTxt: true,
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,11 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
 import { Session } from 'next-auth';
+import SEO from '../../next-seo.config';
 import { FavoriteContextProvider } from '../context/FavoriteContext';
 import { SkeletonTheme } from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
+import { DefaultSeo } from 'next-seo';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps<{ session: Session }>) {
   const [queryClient] = useState(() => new QueryClient());
@@ -19,6 +21,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps<{ s
         <FavoriteContextProvider>
           <Layout>
             <SkeletonTheme baseColor="#282f43" highlightColor="#4F5D75" duration={2}>
+              <DefaultSeo {...SEO} />
               <Component {...pageProps} />
             </SkeletonTheme>
           </Layout>
