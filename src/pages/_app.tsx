e086a988bdb87feaac1f827d42a39e0e6e40c382
Danila Voronkov <danilavoronkov2002@gmail.com>
2022-11-03 15:39:43 +0300

    feat(project): Remove devtools from master

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import Layout from '../components/layout/Layout';
 import { useState } from 'react';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
 import { Session } from 'next-auth';
@@ -25,7 +24,6 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps<{ s
               <Component {...pageProps} />
             </SkeletonTheme>
           </Layout>
-          <ReactQueryDevtools initialIsOpen={false} />
         </FavoriteContextProvider>
       </QueryClientProvider>
     </SessionProvider>
