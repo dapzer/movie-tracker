80f0b780ef754a0fcca8ecb06d354fa085ce7b65
Danila Voronkov <danilavoronkov2002@gmail.com>
2022-11-12 17:40:04 +0300

    feat(details): Remove details title

--- src/components/containers/deteils-page/DetailsPageContainer.tsx
+++ src/components/containers/deteils-page/DetailsPageContainer.tsx
@@ -4,7 +4,6 @@ import Details from '../../../components/core/details/Details';
 import { Details as DetailsType } from '../../../types/Details';
 import { ContentNames } from '../../../types/ContentNames';
 import PersonDetails from '../../core/person-details/PersonDetails';
-import useTranslation from 'next-translate/useTranslation';
 import styles from './details.module.scss';
 import BackBtn from '../../core/back-btn/BackBtn';
 
@@ -15,13 +14,9 @@ interface Props {
 }
 
 const DetailsPageContainer: FC<Props> = ({ mediaType, locale, details }) => {
-  const { t } = useTranslation('card');
-
   return (
     <div className={`container ${styles['details']}`}>
       <BackBtn />
-
-      <h2 className={styles['details__title']}>{t(`about_${mediaType}`)}</h2>
 
       {mediaType === ContentNames.Person ? (
         <PersonDetails initialData={details as Person.RootObject} personId={details.id} />
