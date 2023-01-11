6bb1f911830099a39cdcc54c47d5bb191073e7a6
Dapzer <danilavoronkov2002@gmail.com>
2023-01-11 16:58:11 +0300

    feat(details): Move video overlay on image

--- src/components/details-page/video-card/VideoPreviewCard.tsx
+++ src/components/details-page/video-card/VideoPreviewCard.tsx
@@ -13,23 +13,22 @@ const VideoPreviewCard: FC<Props> = ({ previewUrl, title, releaseDate }) => {
   const { t } = useTranslation('card');
   return (
     <div className={styles['body']}>
-      <div className={styles['overlay']}>
-        <svg>
-          <use href={'/icon-play.svg#svg'}></use>
-        </svg>
-      </div>
-
       <div className={styles['image']}>
         <Image
           src={previewUrl ? previewUrl : '/defaultPoster.svg'}
           blurDataURL={previewUrl ? previewUrl : '/defaultPoster.svg'}
           placeholder='blur'
           width={260}
-          height={150}
+          height={147}
           objectFit='contain'
           sizes='33wv'
           alt='Image'
         />
+        <div className={styles['overlay']}>
+          <svg>
+            <use href={'/icon-play.svg#svg'}></use>
+          </svg>
+        </div>
       </div>
 
       <div className={styles['info']}>
