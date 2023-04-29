eab3023d684ebc6ad4c8f78d0c144a312067b157
Danila Voronkov <danilavoronkov2002@gmail.com>
2023-04-29 15:48:42 +0300

    feat(favoriteBtn): Add ability to change statis after adding in faborite list

--- src/components/favorite-page/tracking-menu/status-selector/StatusSelector.tsx
+++ src/components/favorite-page/tracking-menu/status-selector/StatusSelector.tsx
@@ -4,32 +4,39 @@ import useTranslation from 'next-translate/useTranslation';
 import { useFavorite } from '@/hooks/useFavorite';
 import UiDropdown from '@/components/ui/dropdown/UiDropdown';
 import { FavoriteList } from '@/types/FavoriteList';
+import { ListIcon } from '@/components/ui/Icons';
 
 interface Props {
   mediaType: string;
   id: number;
   currentStatus: FavoriteList.StatusesNames;
+  trigger?: React.ReactNode;
+  dropdownStyles?: string
 }
 
 const statuses: FavoriteList.StatusesNames[] = ['watchingNow', 'notViewed', 'waitNewPart', 'viewed'];
 
-const StatusSelector: FC<Props> = ({ id, mediaType, currentStatus }) => {
+const StatusSelector: FC<Props> = ({ id, mediaType, currentStatus, trigger, dropdownStyles }) => {
   const { t } = useTranslation();
-  const { handleFavorite, changeStatus } = useFavorite(id);
+  const { handleFavorite, changeStatus, isFavorite } = useFavorite(id);
 
   return (
-    <div className={`favorite-btn__movie-card-btn ${styles['content']} ui-dropdown__trigger`}>
-      <span>{t('favoritePage:changeStatus')}</span>
-      <UiDropdown>
-        {statuses.map((el, index) => (
-          <button key={`status-selector-${id}-${index}`} hidden={el === currentStatus} onClick={() => changeStatus(el)}>
-            {t(`favoritePage:statuses.${el}`)}
+    <div className={`${!trigger ? styles['body'] : ''} ${styles['content']} ui-dropdown__trigger`}>
+      {trigger ? trigger : <span><ListIcon /> {t('favoritePage:changeStatus')}</span>}
+      {isFavorite && (
+        <UiDropdown containerClass={dropdownStyles}>
+          {statuses.map((el, index) => (
+            <button key={`status-selector-${id}-${index}`} hidden={el === currentStatus}
+                    onClick={() => changeStatus(el)} className={styles['status']}>
+              {t(`favoritePage:statuses.${el}`)}
+            </button>
+          ))}
+          <button onClick={() => handleFavorite(id, mediaType)}
+                  className={`${styles['status']} ${styles['delete_btn']}`}>
+            {t('buttons:delete_from_favorite')}
           </button>
-        ))}
-        <button onClick={() => handleFavorite(id, mediaType)} className={styles['delete_btn']}>
-          {t('buttons:delete_from_favorite')}
-        </button>
-      </UiDropdown>
+        </UiDropdown>
+      )}
     </div>
   );
 };
