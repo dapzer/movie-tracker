fde26f60e629baa026fafe377655bfe44eb9914f
Danila Voronkov <danilavoronkov2002@gmail.com>
2023-05-06 14:52:51 +0300

    feat(auth): Add prisma for auth

---
+++ src/lib/prisma/index.ts
@@ -0,0 +1,16 @@
+
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+//
+// Learn more:
+// https://pris.ly/d/help/next-js-best-practices
+
+import { PrismaClient } from 'prisma/prisma-client';
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prisma = globalForPrisma.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
