6a353f6c912c7a6e0d851f7bbe780c35d31baf5d
Danila Voronkov <danilavoronkov2002@gmail.com>
2023-09-16 22:01:03 +0300

    refactor(nextConfig): Remove typescript ignore while building

--- next.config.js
+++ next.config.js
@@ -19,13 +19,6 @@ const nextConfig = {
 
     return config;
   },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
 };
 
 module.exports = nextTranslate(nextConfig);
