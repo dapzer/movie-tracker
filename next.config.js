b695e8ed7b203dbdc830c535c608709d7614645d
Danila Voronkov <danilavoronkov2002@gmail.com>
2023-10-11 20:22:55 +0300

    feat: Enable image optimization

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
   images: {
     domains: ['image.tmdb.org', 'sun1-19.userapi.com', 'avatars.yandex.net', 'lh3.googleusercontent.com'],
     deviceSizes: [320, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    unoptimized: true,
+    unoptimized: false,
   },
   webpack(config) {
     config.module.rules.push({
