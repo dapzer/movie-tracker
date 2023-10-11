64333876781f4fb03297fe614344dca6fb0ba04f
Danila Voronkov <danilavoronkov2002@gmail.com>
2023-10-11 22:14:07 +0300

    chore(images): Disable optimization

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
   images: {
     domains: ['image.tmdb.org', 'sun1-19.userapi.com', 'avatars.yandex.net', 'lh3.googleusercontent.com', "i.ytimg.com"],
     deviceSizes: [320, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    unoptimized: false,
+    unoptimized: true,
   },
   webpack(config) {
     config.module.rules.push({
