9f143a4c170a50678ac4d4ffea2aea4166aa6439
Danila Voronkov <danilavoronkov2002@gmail.com>
2023-11-18 19:48:50 +0300

    chore: Migrate to turborepo

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,12 +1,12 @@
 const nextTranslate = require('next-translate');
 
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['image.tmdb.org', 'sun1-19.userapi.com', 'avatars.yandex.net', 'lh3.googleusercontent.com', "i.ytimg.com"],
+    domains: ['image.tmdb.org', 'sun1-19.userapi.com', 'avatars.yandex.net', 'lh3.googleusercontent.com', 'i.ytimg.com'],
     deviceSizes: [320, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     unoptimized: true,
   },
