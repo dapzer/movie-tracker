ef7dbe0470d42247784ca9c6c642157db38a193b
Danila Voronkov <danilavoronkov2002@gmail.com>
2024-02-04 12:50:16 +0300

    fix(api): Change types import in account repository

--- apps/backend/src/repositories/account/PrismaAccountRepository.ts
+++ apps/backend/src/repositories/account/PrismaAccountRepository.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@/services/prisma/prisma.service';
 import {
   AccountRepositoryInterface,
   AccountType,
-} from '@/repositories/Account/AccountRepositoryInterface';
-import { PrismaService } from '@/services/prisma/prisma.service';
+} from '@/repositories/account/AccountRepositoryInterface';
 
 @Injectable()
 export class PrismaAccountRepository implements AccountRepositoryInterface {
@@ -27,14 +27,33 @@ export class PrismaAccountRepository implements AccountRepositoryInterface {
   async createAccount(
     body: Omit<AccountType, 'id' | 'createdAt' | 'updatedAt'>,
   ) {
-    return this.prisma.account.create({ data: body });
+    return this.prisma.account.create({
+      data: {
+        provider: body.provider,
+        providerAccountId: body.providerAccountId,
+        access_token: body.access_token,
+        refresh_token: body.refresh_token,
+        expires_at: body.expires_at,
+        userId: body.userId,
+        type: body.type,
+      },
+    });
   }
 
   async updateAccount(
     id: string,
     body: Omit<AccountType, 'id' | 'createdAt' | 'updatedAt'>,
   ) {
-    return this.prisma.account.update({ where: { id }, data: body });
+    return this.prisma.account.update({
+      where: { id },
+      data: {
+        provider: body.provider,
+        providerAccountId: body.providerAccountId,
+        access_token: body.access_token,
+        refresh_token: body.refresh_token,
+        expires_at: body.expires_at,
+      },
+    });
   }
 
   async deleteAccount(id: string) {
