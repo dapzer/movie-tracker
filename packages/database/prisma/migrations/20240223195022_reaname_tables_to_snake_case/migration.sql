8938435a4c802de2bc1ab2fb2ee43845771688e9
Danila Voronkov <danilavoronkov2002@gmail.com>
2024-02-23 22:59:38 +0300

    chore(api): Rename tables to snake case

---
+++ packages/database/prisma/migrations/20240223195022_reaname_tables_to_snake_case/migration.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "mediaLists" RENAME TO "media_lists";
+ALTER TABLE "mediaItems" RENAME TO "media_items";
+ALTER TABLE "trackingData" RENAME TO "tracking_data";
+ALTER TABLE "mediaDetails" RENAME TO "media_details";
