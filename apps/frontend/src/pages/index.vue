fbeca9f74fb5c599402ea796602e720afe003091
Danila Voronkov <danilavoronkov2002@gmail.com>
2024-05-21 21:50:33 +0300

    chore(seo): Remove breadcrumbs chema

--- apps/frontend/src/pages/index.vue
+++ apps/frontend/src/pages/index.vue
@@ -1,25 +1,12 @@
 <script lang="ts" setup>
-import { Hero } from "~/widgets/hero";
-import { SearchContent } from "~/features/search";
-import { computed, defineBreadcrumb, useI18n, useSchemaOrg } from "#imports";
-import { searchStore } from "~/stores/searcStore";
-import { isOnlySpaces } from "@movie-tracker/utils";
-import { PopularLists } from "~/widgets/popularLists";
-import { useLocalePath } from "#i18n";
-
-const { t } = useI18n();
-const localePath = useLocalePath();
+import { Hero } from '~/widgets/hero';
+import { SearchContent } from '~/features/search';
+import { computed } from '#imports';
+import { searchStore } from '~/stores/searcStore';
+import { isOnlySpaces } from '@movie-tracker/utils';
+import { PopularLists } from '~/widgets/popularLists';
 
 const searchTerm = computed(() => searchStore.state.searchValue);
-
-useSchemaOrg([
-  defineBreadcrumb({
-    itemListElement: [
-      { name: t("navigation.lists"), item: localePath("/lists") }
-    ]
-  })
-]);
-
 </script>
 
 <template>
