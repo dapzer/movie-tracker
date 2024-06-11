88059685c2aec6b1fb02e9b63903fd08b188cac0
Danila Voronkov <danilavoronkov2002@gmail.com>
2024-06-11 23:06:27 +0300

    chore(seo): Change meta tags

--- apps/frontend/src/features/details/model/usePersonDetailsSeo.ts
+++ apps/frontend/src/features/details/model/usePersonDetailsSeo.ts
@@ -1,43 +1,33 @@
-import type { TmdbPersonType } from "@movie-tracker/types";
-import { definePerson, getProxiedImageUrl, useI18n, useSchemaOrg, useSeoMeta } from "#imports";
-import { useLocalePath } from "#i18n";
-import { generateApiUrl } from "@movie-tracker/utils";
-import { computed } from "vue";
+import type { TmdbPersonType } from '@movie-tracker/types';
+import { getProxiedImageUrl, useI18n, useSeoMeta } from '#imports';
+import { generateApiUrl } from '@movie-tracker/utils';
+import { computed } from 'vue';
 
 export const usePersonDetailsSeo = (person?: TmdbPersonType | null) => {
   const { t } = useI18n();
-  const localePath = useLocalePath();
-  const getOgApiUrl = generateApiUrl(import.meta.env.VITE_API_URL || "");
+  const getOgApiUrl = generateApiUrl(import.meta.env.VITE_API_URL || '');
 
   const ogImage = computed(() => {
     return getOgApiUrl(`/open-graph-image`, {
-      title: person?.name || "",
+      title: person?.name || '',
       imageUrl: getProxiedImageUrl(person?.profile_path, undefined, true),
     });
   });
+
   useSeoMeta({
     titleTemplate(titleChunk) {
-      return `${titleChunk} | ${person?.name}`;
+      return `${person?.name} | ${person?.known_for_department ? `${t(`details.knowFor.${person?.known_for_department}`,
+      )} | ` : ''}${titleChunk}`;
     },
     ogTitle: `%s | ${person?.name}`,
-    description: person?.biography || t("seo.description"),
-    ogDescription: person?.biography || t("seo.description"),
+    description: person?.biography || t('seo.description'),
+    ogDescription: person?.biography || t('seo.description'),
     ogImageWidth: 1200,
     ogImageHeight: 630,
     ogImage: ogImage.value,
     twitterImage: ogImage.value,
-    twitterCard: "summary_large_image",
+    twitterCard: 'summary_large_image',
     twitterTitle: `%s | ${person?.name}`,
-    twitterDescription: person?.biography || t("seo.description")
+    twitterDescription: person?.biography || t('seo.description'),
   });
-
-  // useSchemaOrg([
-  //   definePerson({
-  //     name: person?.name,
-  //     url: localePath(`/details/person/${person?.id}`),
-  //     description: person?.biography,
-  //     image: getProxiedImageUrl(person?.profile_path)
-  //   })
-  // ]);
-
 };
