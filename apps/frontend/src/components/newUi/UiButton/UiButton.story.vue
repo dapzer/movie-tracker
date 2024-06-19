77b7a11f967ab96ab80f2131112e5000b4d79bde
dapzer <danilavoronkov2002@gmail.com>
2024-06-20 00:38:55 +0300

    fix(ui): Redesign button component

---
+++ apps/frontend/src/components/newUi/UiButton/UiButton.story.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+
+import UiButton from "~/components/newUi/UiButton/UiButton.vue"
+import { ref } from "vue"
+
+const disabled = ref(false)
+</script>
+
+<template>
+  <Story
+    title="UiButton"
+    :layout="{ type: 'grid', width: 150 }"
+  >
+    <template #default>
+      <Variant title="Primary">
+        <UiButton
+          variant="primary"
+          :disabled="disabled"
+        >
+          Button
+        </UiButton>
+      </Variant>
+
+      <Variant title="Secondary">
+        <UiButton
+          variant="secondary"
+          :disabled="disabled"
+        >
+          Button
+        </UiButton>
+      </variant>
+
+      <Variant title="Tertiary">
+        <UiButton
+          variant="tertiary"
+          :disabled="disabled"
+        >
+          Button
+        </UiButton>
+      </Variant>
+    </template>
+
+    <template #controls>
+      <HstCheckbox
+        v-model="disabled"
+        title="Disabled"
+      />
+    </template>
+  </Story>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ apps/frontend/src/components/newUi/UiButton/UiButton.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+export type UiButtonVariant = "primary" | "secondary" | "tertiary"
+
+interface UiButtonProps {
+  variant?: UiButtonVariant
+}
+
+const props = withDefaults(defineProps<UiButtonProps>(), {
+  variant: "primary"
+})
+</script>
+
+<template>
+  <button
+    :class="[$style.default, {
+      [$style.primary]: props.variant === 'primary',
+      [$style.secondary]: props.variant === 'secondary',
+      [$style.tertiary]: props.variant === 'tertiary',
+    }]"
+  >
+    <slot />
+  </button>
+</template>
+
+<style module lang="scss">
+.default {
+  background: unset;
+  border: unset;
+  outline: unset;
+  cursor: pointer;
+  font-family: var(--ff-inter);
+  font-size: var(--fs-label);
+  line-height: var(--lh-label);
+  width: fit-content;
+
+  &:disabled {
+    pointer-events: none;
+    opacity: var(--s-disabled-opacity);
+  }
+}
+
+.primary {
+  color: var(--c-text);
+  background: var(--c-button-background-primary);
+  padding: 12px 24px;
+  border-radius: var(--s-border-radius-medium);
+
+  &:active,
+  &:hover {
+    background: var(--c-button-background-primary-hovered);
+  }
+}
+
+.secondary {
+  color: var(--c-label-secondary);
+  background: var(--c-button-background-secondary);
+  padding: 8px 12px;
+  border-radius: var(--s-border-radius-medium);
+
+  &:active,
+  &:hover {
+    background: var(--c-button-background-secondary-hovered);
+  }
+}
+
+.tertiary {
+  color: var(--c-label-tertiary);
+  background: var(--c-button-background-tertiary);
+  padding: 8px 20px;
+  border-radius: var(--s-border-radius-medium);
+
+  &:active,
+  &:hover {
+    background: var(--c-button-background-tertiary-hovered);
+  }
+}
+</style>
