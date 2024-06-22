7953433535ac0b311ef34820ec1a99e3695b7fd5
dapzer <danilavoronkov2002@gmail.com>
2024-06-22 22:40:51 +0300

    feat(ui): Redesign switch component

---
+++ apps/frontend/src/components/newUi/UiSwitch/UiSwitch.story.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+import { UiSwitch } from "~/components/newUi/UiSwitch"
+import { ref } from "vue"
+
+const disabled = ref(false)
+const checked = ref(false)
+</script>
+
+<template>
+  <Story title="UiSwitch">
+    <template #default>
+      <UiSwitch
+        v-model="checked"
+        :disabled="disabled"
+      />
+    </template>
+
+    <template #controls>
+      <HstCheckbox
+        v-model="checked"
+        title="Checked"
+      />
+      <HstCheckbox
+        v-model="disabled"
+        title="Disabled"
+      />
+    </template>
+  </Story>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ apps/frontend/src/components/newUi/UiSwitch/index.ts
@@ -0,0 +1,1 @@
+export { default as UiSwitch } from "./UiSwitch.vue"
