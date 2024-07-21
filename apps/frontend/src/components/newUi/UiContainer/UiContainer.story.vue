ce99abfeb07eda438b917c33e6ae7295f97b55ef
dapzer <danilavoronkov2002@gmail.com>
2024-07-21 16:13:05 +0300

    feat(ui): Redesign container component

---
+++ apps/frontend/src/components/newUi/UiContainer/UiContainer.story.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+import UiContainer from "~/components/newUi/UiContainer/UiContainer.vue"
+</script>
+
+<template>
+  <Story title="UiContainer">
+    <template #default>
+      <UiContainer>
+        <div :class="$style.content" />
+      </UiContainer>
+    </template>
+  </Story>
+</template>
+
+<style module lang="scss">
+.content {
+  width: 100%;
+  height: 50px;
+  background: var(--c-card-background-hovered);
+}
+</style>
---
+++ apps/frontend/src/components/newUi/UiContainer/index.ts
@@ -0,0 +1,1 @@
+export { default as UiContainer } from './UiContainer.vue';
