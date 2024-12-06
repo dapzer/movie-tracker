b5fccbb314073f403e666fc94c6e0a5a5830f24b
dapzer <danilavoronkov2002@gmail.com>
2024-12-06 11:56:36 +0300

    fix(futureReleases): Add cards render

--- apps/frontend/src/pages/upcoming/movies.vue
+++ apps/frontend/src/pages/upcoming/movies.vue
@@ -9,6 +9,7 @@ import type { TmdbDiscoverMovieQueriesType } from "~/api/tmdb/tmdbApiTypes"
 import { nextThirtyDaysWithoutTime, todayWithoutTime } from "~/shared/constants/dates"
 import { getTmdbTotalPages } from "~/utils/getTmdbTotalPages"
 import { MediaTypeEnum } from "@movie-tracker/types"
+import { MovieCardWithHoverMenu } from "~/features/movieCardWithHoverMenu"
 
 const { locale, t } = useI18n();
 const currentPage = ref(1)
