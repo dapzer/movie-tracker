50ba42a20a804c6a3907e90018c15faa64194375
dapzer <danilavoronkov2002@gmail.com>
2024-12-08 12:45:29 +0300

    fix(footer): Hide discord from social list

--- apps/frontend/src/shared/ui/publicResources/model/publicResourcesList.ts
+++ apps/frontend/src/shared/ui/publicResources/model/publicResourcesList.ts
@@ -1,13 +1,16 @@
-import { DiscordIcon, GithubIcon, TelegramIcon } from "~/components/ui/icons"
+import { GithubIcon, TelegramIcon } from "~/components/ui/icons"
 
 export const publicResourcesList = [
+  // TODO: Add Discord link
+  // {
+  //   icon: DiscordIcon,
+  //   url: "",
+  // },
   {
-    icon: DiscordIcon,
-    url: "",
-  }, {
     icon: GithubIcon,
     url: "https://github.com/dapzer/movie-tracker",
-  }, {
+  },
+  {
     icon: TelegramIcon,
     url: "https://t.me/movie_tracker_news",
   }
