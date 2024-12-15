f66d51727359b42b26c207c48199e4713d5c5cf4
dapzer <danilavoronkov2002@gmail.com>
2024-12-15 14:51:05 +0300

    feat(api): Add uptrace

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,6 +1,7 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
 import 'dotenv/config';
+import "@/services/opentelemetry"
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
