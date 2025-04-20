de6e4ec5cf2b32c64047363ea981c29bf5c572a7
dapzer <danilavoronkov2002@gmail.com>
2025-04-20 12:34:39 +0300

    feat(ui): Add limit component

---
+++ apps/frontend/src/shared/ui/UiiLimit/UiiLimit.story.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import UiiLimit from "~/shared/ui/UiiLimit/UiiLimit.vue"
+</script>
+
+<template>
+  <Story
+    title="UiiLimit"
+    group="ui-kit"
+  >
+    <template #default>
+      <UiiLimit
+        :limit="10"
+        :current="5"
+        :is-loading="false"
+      />
+    </template>
+  </Story>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ apps/frontend/src/shared/ui/UiiLimit/index.ts
@@ -0,0 +1,1 @@
+export { default as UiiLimit } from "./UiiLimit.vue"
