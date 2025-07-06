a9bbd48e40b01fa72f2e7b03302fb1e203a45c38
dapzer <danilavoronkov2002@gmail.com>
2025-07-06 17:33:36 +0300

    feat(communityListsApi): add options parameter for API requests

--- apps/frontend/src/api/communityLists/communityListsApi.ts
+++ apps/frontend/src/api/communityLists/communityListsApi.ts
@@ -6,44 +6,50 @@ import type {
   GetCommunityListsWithMediaQueries,
   MediaListsPaginatedType,
 } from "@movie-tracker/types"
+import type { RequestOptions } from "@movie-tracker/utils"
 import { api } from "~/api/instance"
 
-export function getCommunityListsWeekTopApi(args: { queries?: GetCommunityListsWeekTopQueries }) {
+export function getCommunityListsWeekTopApi(args: { queries?: GetCommunityListsWeekTopQueries, options: RequestOptions }) {
   return api.get<MediaListsPaginatedType>("community-lists/week-top", {
     params: {
       ...args.queries,
     },
+    ...args.options,
   })
 }
 
-export function getCommunityListsAllTimeTopApi(args: { queries?: GetCommunityListsAllTimeTopQueries }) {
+export function getCommunityListsAllTimeTopApi(args: { queries?: GetCommunityListsAllTimeTopQueries, options: RequestOptions }) {
   return api.get<MediaListsPaginatedType>("community-lists/all-time-top", {
     params: {
       ...args.queries,
     },
+    ...args.options,
   })
 }
 
-export function getCommunityListsNewestApi(args: { queries?: GetCommunityListsNewestQueries }) {
+export function getCommunityListsNewestApi(args: { queries?: GetCommunityListsNewestQueries, options: RequestOptions }) {
   return api.get<MediaListsPaginatedType>("community-lists/newest", {
     params: {
       ...args.queries,
     },
+    ...args.options,
   })
 }
 
-export function getCommunityListsSearchApi(args: { queries?: GetCommunityListsSearchQueries }) {
+export function getCommunityListsSearchApi(args: { queries?: GetCommunityListsSearchQueries, options: RequestOptions }) {
   return api.get<MediaListsPaginatedType>("community-lists/search", {
     params: {
       ...args.queries,
     },
+    ...args.options,
   })
 }
 
-export function getCommunityListsWithMediaApi(args: { queries?: GetCommunityListsWithMediaQueries }) {
+export function getCommunityListsWithMediaApi(args: { queries?: GetCommunityListsWithMediaQueries, options: RequestOptions }) {
   return api.get<MediaListsPaginatedType>("community-lists/with-media", {
     params: {
       ...args.queries,
     },
+    ...args.options,
   })
 }
--- apps/frontend/src/api/communityLists/useCommunityListsApi.ts
+++ apps/frontend/src/api/communityLists/useCommunityListsApi.ts
@@ -6,6 +6,7 @@ import type {
   GetCommunityListsWithMediaQueries,
 } from "@movie-tracker/types"
 import type { Ref } from "vue"
+import { useRequestHeaders } from "#app"
 import { useQuery } from "@tanstack/vue-query"
 import {
   getCommunityListsAllTimeTopApi,
@@ -19,34 +20,49 @@ import { CommunityListsApiQueryKeys } from "./communityListsApiQueryKeys"
 export function useCommunityListsWeekTopApi(queries?: Ref<GetCommunityListsWeekTopQueries>) {
   return useQuery({
     queryKey: [CommunityListsApiQueryKeys.GET_WEEK_TOP, queries],
-    queryFn: () => getCommunityListsWeekTopApi({ queries: queries?.value }),
+    queryFn: () => {
+      const headers = useRequestHeaders(["cookie"])
+      return getCommunityListsWeekTopApi({ queries: queries?.value, options: { headers } })
+    },
   })
 }
 
 export function useCommunityListsAllTimeTopApi(queries?: Ref<GetCommunityListsAllTimeTopQueries>) {
   return useQuery({
     queryKey: [CommunityListsApiQueryKeys.GET_ALL_TIME_TOP, queries],
-    queryFn: () => getCommunityListsAllTimeTopApi({ queries: queries?.value }),
+    queryFn: () => {
+      const headers = useRequestHeaders(["cookie"])
+      return getCommunityListsAllTimeTopApi({ queries: queries?.value, options: { headers } })
+    },
   })
 }
 
 export function useCommunityListsNewestApi(queries?: Ref<GetCommunityListsNewestQueries>) {
   return useQuery({
     queryKey: [CommunityListsApiQueryKeys.GET_NEWEST, queries],
-    queryFn: () => getCommunityListsNewestApi({ queries: queries?.value }),
+    queryFn: () => {
+      const headers = useRequestHeaders(["cookie"])
+      return getCommunityListsNewestApi({ queries: queries?.value, options: { headers } })
+    },
   })
 }
 
 export function useCommunityListsSearchApi(queries?: Ref<GetCommunityListsSearchQueries>) {
   return useQuery({
     queryKey: [CommunityListsApiQueryKeys.GET_SEARCH, queries],
-    queryFn: () => getCommunityListsSearchApi({ queries: queries?.value }),
+    queryFn: () => {
+      const headers = useRequestHeaders(["cookie"])
+      return getCommunityListsSearchApi({ queries: queries?.value, options: { headers } })
+    },
   })
 }
 
 export function useCommunityListsWithMediaApi(queries?: Ref<GetCommunityListsWithMediaQueries>) {
   return useQuery({
     queryKey: [CommunityListsApiQueryKeys.GET_WITH_MEDIA, queries],
-    queryFn: () => getCommunityListsWithMediaApi({ queries: queries?.value }),
+    queryFn: () => {
+      const headers = useRequestHeaders(["cookie"])
+      return getCommunityListsWithMediaApi({ queries: queries?.value, options: { headers } })
+    },
   })
 }
