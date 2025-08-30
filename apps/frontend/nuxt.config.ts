4387f686055175058e3a5befce8ebb6c6700eb1e
dapzer <danilavoronkov2002@gmail.com>
2025-08-30 17:14:25 +0300

    chore: update global styles path in nuxt.config.ts and import in app.vue

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -108,7 +108,7 @@ export default defineNuxtConfig({
     }],
   },
 
-  css: ["./app/shared/styles/global.scss"],
+  // css: ["~/shared/styles/global.scss"],
 
   imports: {
     autoImport: false,
