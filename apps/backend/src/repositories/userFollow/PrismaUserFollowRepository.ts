fa6971a4b4180c63fa7d9dcc94ff1e56c28f205d
dapzer <danilavoronkov2002@gmail.com>
2025-11-01 16:39:39 +0300

    feat(userFollow): implement prisma repository

---
+++ apps/backend/src/repositories/userFollow/PrismaUserFollowRepository.ts
@@ -0,0 +1,135 @@
+import { UserFollow } from "@movie-tracker/database"
+import { UserFollowProfileType, UserFollowType } from "@movie-tracker/types"
+import { Injectable } from "@nestjs/common"
+import { UserFollowRepositoryInterface } from "@/repositories/userFollow/UserFollowRepositoryInterface"
+import { PrismaService } from "@/services/prisma/prisma.service"
+
+interface FollowProfile {
+  name: string
+  id: string
+  image: string
+  _count: {
+    userFollowers: number
+  }
+}
+
+@Injectable()
+export class PrismaUserFollowRepository implements UserFollowRepositoryInterface {
+  constructor(private readonly prisma: PrismaService) {}
+
+  private convertFollowProfileToInterface = (profile: FollowProfile | undefined): UserFollowProfileType => {
+    if (!profile) {
+      return undefined
+    }
+
+    return {
+      id: profile.id,
+      name: profile.name,
+      image: profile.image,
+      followersCount: profile._count.userFollowers,
+    }
+  }
+
+  private convertToInterface
+    = (userFollow: UserFollow & { follower?: FollowProfile, following?: FollowProfile } | null,
+    ): UserFollowType => {
+      if (!userFollow) {
+        return undefined
+      }
+
+      return {
+        id: userFollow.id,
+        followerId: userFollow.followerId,
+        followingId: userFollow.followingId,
+        createdAt: userFollow.createdAt,
+        followerUserProfile: this.convertFollowProfileToInterface(userFollow.follower),
+        followingUserProfile: this.convertFollowProfileToInterface(userFollow.following),
+      }
+    }
+
+  async createFollow(args: Parameters<UserFollowRepositoryInterface["createFollow"]>[0]) {
+    const userFollow = await this.prisma.userFollow.create({
+      data: {
+        followerId: args.followerId,
+        followingId: args.followingUserId,
+      },
+    })
+
+    return this.convertToInterface(userFollow)
+  }
+
+  async deleteFollow(args: Parameters<UserFollowRepositoryInterface["deleteFollow"]>[0]) {
+    const userFollow = await this.prisma.userFollow.deleteMany({
+      where: {
+        followerId: args.followerId,
+        followingId: args.followingUserId,
+      },
+    })
+
+    return this.convertToInterface(userFollow[0])
+  }
+
+  async getFollowers(args: Parameters<UserFollowRepositoryInterface["getFollowers"]>[0]) {
+    const userFollowers = await this.prisma.userFollow.findMany({
+      where: {
+        followingId: args.userId,
+      },
+      include: {
+        follower: {
+          select: {
+            id: true,
+            name: true,
+            image: true,
+            _count: {
+              select: {
+                userFollowers: true,
+              },
+            },
+          },
+        },
+      },
+    })
+
+    return userFollowers.map(this.convertToInterface)
+  }
+
+  getFollowersCount(args: Parameters<UserFollowRepositoryInterface["getFollowersCount"]>[0]) {
+    return this.prisma.userFollow.count({
+      where: {
+        followingId: args.userId,
+      },
+    })
+  }
+
+  async getFollowing(args: Parameters<UserFollowRepositoryInterface["getFollowing"]>[0]) {
+    const userFollowing = await this.prisma.userFollow.findMany({
+      where: {
+        followerId: args.userId,
+      },
+      include: {
+        following: {
+          select: {
+            id: true,
+            name: true,
+            image: true,
+            _count: {
+              select: {
+                userFollowers: true,
+              },
+            },
+          },
+        },
+      },
+    })
+
+    return userFollowing.map(this.convertToInterface)
+  }
+
+  getFollowingCount(args: Parameters<UserFollowRepositoryInterface["getFollowingCount"]>[0]) {
+    return this.prisma.userFollow.count({
+      where: {
+        followerId: args.userId,
+      },
+    })
+  }
+}
---
+++ apps/backend/src/repositories/userFollow/UserFollowRepositoryInterface.ts
@@ -0,0 +1,12 @@
+import { UserFollowType } from "@movie-tracker/types"
+
+export const UserFollowRepositorySymbol = Symbol("UserFollowRepository")
+
+export interface UserFollowRepositoryInterface {
+  createFollow: (args: { followerId: string, followingUserId: string }) => Promise<UserFollowType>
+  deleteFollow: (args: { followerId: string, followingUserId: string }) => Promise<UserFollowType>
+  getFollowers: (args: { userId: string }) => Promise<UserFollowType[]>
+  getFollowersCount: (args: { userId: string }) => Promise<number>
+  getFollowing: (args: { userId: string }) => Promise<UserFollowType[]>
+  getFollowingCount: (args: { userId: string }) => Promise<number>
+}
