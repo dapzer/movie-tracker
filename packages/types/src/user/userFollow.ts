bcf0c304093ce40224f4c721cc5593e3fb15556a
dapzer <danilavoronkov2002@gmail.com>
2025-11-01 16:39:08 +0300

    feat(userFollow): add types

---
+++ packages/types/src/user/userFollow.ts
@@ -0,0 +1,14 @@
+import { UserType } from "./user"
+
+export interface UserFollowType {
+  id: string
+  followerId: string
+  followingId: string
+  createdAt: Date
+  followerUserProfile?: UserFollowProfileType
+  followingUserProfile?: UserFollowProfileType
+}
+
+export type UserFollowProfileType = Pick<UserType, "id" | "name" | "image"> & {
+  followersCount: number
+}
