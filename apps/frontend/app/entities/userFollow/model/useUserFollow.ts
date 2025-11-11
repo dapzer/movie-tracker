0a64a4e39ebc7d67c8d3898c2e8f5b3561212da2
dapzer <danilavoronkov2002@gmail.com>
2025-11-11 20:16:57 +0300

    fix(userFollow): redirect if isn't authorized to sign in page on follow

--- apps/frontend/app/entities/userFollow/model/useUserFollow.ts
+++ apps/frontend/app/entities/userFollow/model/useUserFollow.ts
@@ -2,14 +2,23 @@ import { useI18n } from "#imports"
 import { computed } from "vue"
 import { toast } from "vue3-toastify"
 import { useCreateUserFollowApi, useDeleteUserFollowApi } from "~/api/userFollow/useUserFollowApi"
+import { useAuth } from "~/shared/composables/useAuth"
+import { useNavigateToSignInPage } from "~/shared/composables/useNavigateToSignInPage"
 
 export function useUserFollow() {
   const { t } = useI18n()
 
   const createUserFollowApi = useCreateUserFollowApi()
   const deleteUserFollowApi = useDeleteUserFollowApi()
+  const { isAuthorized } = useAuth()
+  const { navigateToSignInPage } = useNavigateToSignInPage()
 
   async function handleFollow(args: { isFollowing: boolean, userId: string }) {
+    if (!isAuthorized.value) {
+      navigateToSignInPage()
+      return
+    }
+
     if (args.isFollowing) {
       await deleteUserFollowApi.mutateAsync(args.userId).then(() => {
         toast.success(t("toasts.follow.successUnfollowed"))
