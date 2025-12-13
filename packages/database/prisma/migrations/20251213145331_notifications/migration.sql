6297b594ac839d3b1d7ddfd5b181d70c317f09c9
dapzer <danilavoronkov2002@gmail.com>
2025-12-13 17:59:45 +0300

    feat(database): notification table migration

--- packages/database/prisma/migrations/20251213145331_notifications/migration.sql
+++ packages/database/prisma/migrations/20251213145331_notifications/migration.sql
@@ -4,7 +4,7 @@ CREATE TYPE "NotificationTypeEnum" AS ENUM ('MEDIA_LIST_LIKE', 'USER_FOLLOW');
 -- CreateTable
 CREATE TABLE "notifications" (
     "id" TEXT NOT NULL,
-    "user_id" TEXT NOT NULL,
+    "user_id" TEXT,
     "type" "NotificationTypeEnum" NOT NULL,
     "meta" JSONB NOT NULL,
     "read_at" TIMESTAMP(3),
@@ -12,6 +12,9 @@ CREATE TABLE "notifications" (
 
     CONSTRAINT "notifications_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE INDEX "notifications_meta_idx" ON "notifications" USING HASH ("meta");
 
 -- AddForeignKey
 ALTER TABLE "notifications" ADD CONSTRAINT "notifications_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
