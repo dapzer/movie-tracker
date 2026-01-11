e795d4c707a2ef77afad6de13cc44641ff4f7015
dapzer <danilavoronkov2002@gmail.com>
2026-01-11 22:36:53 +0300

    feat(releaseSubscription): add types

---
+++ packages/types/src/media/releaseSubscription.ts
@@ -0,0 +1,22 @@
+import type { MediaDetailsType } from "./mediaDetails"
+
+export interface ReleaseSubscriptionType {
+  id: string
+  mediaDetailsId: string
+  userId: string
+  completedAt: Date | null
+  createdAt: Date
+}
+
+export interface ReleaseSubscriptionWithDetailsType extends ReleaseSubscriptionType {
+  mediaDetails: MediaDetailsType
+}
+
+export interface ReleaseSubscriptionsResponseType {
+  items: ReleaseSubscriptionWithDetailsType[]
+  totalCount: number
+}
+
+export type CreateReleaseSubscriptionType = Pick<ReleaseSubscriptionType, "userId" | "mediaDetailsId">
+
+export type UpdateReleaseSubscriptionType = Pick<ReleaseSubscriptionType, "completedAt">
