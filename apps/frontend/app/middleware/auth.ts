6591a22218952ebb91f0a647f92182d4427ef7af
dapzer <danilavoronkov2002@gmail.com>
2026-02-01 18:46:47 +0300

    feat(auth): use with context for auth middleware

--- apps/frontend/app/middleware/auth.ts
+++ apps/frontend/app/middleware/auth.ts
@@ -1,18 +1,20 @@
-import { defineNuxtRouteMiddleware, navigateTo } from "#app"
+import { defineNuxtRouteMiddleware, navigateTo, useNuxtApp } from "#app"
 import { useGetUserProfileApi } from "~/api/user/useUserApi"
 
 export default defineNuxtRouteMiddleware(async (to) => {
-  const user = useGetUserProfileApi()
-  if (!user.isFetched.value) {
-    await user.suspense()
-  }
+  const nuxtApp = useNuxtApp()
 
-  if (!user.data.value) {
+  const user = await nuxtApp.runWithContext(async () => {
+    const getUserProfileApi = useGetUserProfileApi()
+    return getUserProfileApi.suspense()
+  })
+
+  if (!user.data) {
     return navigateTo("/sign-in", { redirectCode: 401 })
   }
 
   if (to.meta.requiredRoles) {
-    const hasRequiredRole = to.meta.requiredRoles.some(role => user.data?.value?.roles.includes(role))
+    const hasRequiredRole = to.meta.requiredRoles.some(role => user.data?.roles.includes(role))
 
     if (!hasRequiredRole) {
       return navigateTo("/", { redirectCode: 403 })
