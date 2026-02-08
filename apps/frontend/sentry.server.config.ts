13057e644fd2edc293e568095608b1c5d39e7ea3
dapzer <danilavoronkov2002@gmail.com>
2026-02-08 15:13:23 +0300

    feat(sentry): update Sentry configuration to use runtime environment variable

--- apps/frontend/sentry.server.config.ts
+++ apps/frontend/sentry.server.config.ts
@@ -1,8 +1,7 @@
-import * as process from "node:process"
 import * as Sentry from "@sentry/nuxt"
 
 Sentry.init({
-  dsn: process.env.NUXT_PUBLIC_SENTRY_DSN,
+  dsn: import.meta.env.NUXT_PUBLIC_SENTRY_DSN,
 
   // We recommend adjusting this value in production, or using tracesSampler
   // for finer control
