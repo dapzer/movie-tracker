115e030b9023704e20a564f863d4b391d5b0e736
dapzer <danilavoronkov2002@gmail.com>
2026-02-08 16:15:34 +0300

    feat(sentry): update Sentry DSN configuration and add auth token to Dockerfile

--- apps/frontend/sentry.server.config.ts
+++ apps/frontend/sentry.server.config.ts
@@ -1,8 +1,7 @@
 import * as Sentry from "@sentry/nuxt"
 
 Sentry.init({
-  // eslint-disable-next-line node/prefer-global/process
-  dsn: process.env.NUXT_PUBLIC_SENTRY_DSN,
+  dsn: import.meta.env.NUXT_PUBLIC_SENTRY_DSN,
 
   // We recommend adjusting this value in production, or using tracesSampler
   // for finer control
