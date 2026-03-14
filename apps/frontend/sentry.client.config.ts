ebc2a74537bf7db0653f9890ea9a0abedac1b29f
dapzer <danilavoronkov2002@gmail.com>
2026-03-14 17:51:05 +0300

    refactor(sentry): update Sentry configuration to lazy load replay integration

--- apps/frontend/sentry.client.config.ts
+++ apps/frontend/sentry.client.config.ts
@@ -2,25 +2,19 @@ import { useRuntimeConfig } from "#imports"
 import * as Sentry from "@sentry/nuxt"
 
 Sentry.init({
-  // If set up, you can use your runtime config here
-  // dsn: useRuntimeConfig().public.sentry.dsn,
   dsn: useRuntimeConfig().public.sentry.dsn,
 
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
   tracesSampleRate: 1.0,
 
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
   replaysSessionSampleRate: 0.3,
 
-  // If the entire session is not sampled, use the below sample rate to sample
-  // sessions when an error occurs.
   replaysOnErrorSampleRate: 1.0,
 
-  // If you don't want to use Session Replay, just remove the line below:
-  integrations: [Sentry.replayIntegration()],
+  integrations: [],
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+})
+
+import("@sentry/nuxt").then((lazyLoadedSentry) => {
+  Sentry.addIntegration(lazyLoadedSentry.replayIntegration())
 })
