ea55cb88004cd9e22a0e1acf8568e769a1ebe2d3
Andrii Zontov <loginmanx@gmail.com>
2026-04-25 17:32:13 +0200

    chore(backend): Remove `@IsString`

--- apps/backend/src/services/users/dto/update-user-language.dto.ts
+++ apps/backend/src/services/users/dto/update-user-language.dto.ts
@@ -1,8 +1,7 @@
 import { LanguagesEnum } from "@movie-tracker/types"
-import { IsEnum, IsOptional, IsString } from "class-validator"
+import { IsEnum, IsOptional } from "class-validator"
 
 export class UpdateUserLanguageDto {
-  @IsString()
   @IsOptional()
   @IsEnum(LanguagesEnum)
   language?: LanguagesEnum
