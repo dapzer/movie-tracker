599ce11c23c50281212f7872d7ab2d2bce6bf633
dapzer <danilavoronkov2002@gmail.com>
2026-04-26 15:42:31 +0300

    fix(config): disable telemetry in Nuxt configuration

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -3,6 +3,8 @@ import process from "node:process"
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  telemetry: false,
+
   app: {
     head: {
       title: "Movie Tracker",
