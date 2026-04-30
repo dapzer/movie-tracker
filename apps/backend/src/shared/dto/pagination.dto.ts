e8107ad582a60a9735a8acdc85d8d93f23c0dfd6
Misha <darkrabbit240@gmail.com>
2026-04-30 14:41:20 +0300

    feat(openapi): add docs for shared dto

--- apps/backend/src/shared/dto/pagination.dto.ts
+++ apps/backend/src/shared/dto/pagination.dto.ts
@@ -1,13 +1,16 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { Type } from "class-transformer"
 import { IsNumber, IsOptional, Max, Min } from "class-validator"
 
 export class PaginationDto {
+  @ApiProperty({ type: Number, example: 4 })
   @Type(() => Number)
   @IsNumber()
   @Min(0)
   @IsOptional()
   offset: number = 0
 
+  @ApiProperty({ type: Number, example: 4 })
   @Type(() => Number)
   @IsNumber()
   @Min(1)
