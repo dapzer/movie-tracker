20c967f318de19385c94bb1c8884afaa21bfd25d
dapzer <danilavoronkov2002@gmail.com>
2026-05-16 22:05:53 +0300

    refactor(auth): remove unused imports and commented-out code in auth.controller.ts

--- apps/backend/src/delivery/http/auth/auth.controller.ts
+++ apps/backend/src/delivery/http/auth/auth.controller.ts
@@ -1,39 +1,14 @@
-import { Body, Controller, Get, HttpStatus, Param, Patch, Post, Query, Req, Res, UseGuards } from "@nestjs/common"
+import { Controller, Get, HttpStatus, Param, Post, Query, Req, Res, UseGuards } from "@nestjs/common"
 import { ConfigService } from "@nestjs/config"
-import { Throttle } from "@nestjs/throttler"
 import { Request, Response } from "express"
 import { AuthService } from "@/services/auth/auth.service"
 import { AllowedProvider } from "@/services/auth/dto/allowedProvider"
-import { ConfirmChangeEmailDto } from "@/services/auth/dto/confirmChangeEmail.dto"
-import { ConfirmEmailDto } from "@/services/auth/dto/confirmEmail.dto"
-import { GetRecoverPasswordEmailDto } from "@/services/auth/dto/getRecoverPasswordEmail.dto"
-import { RequestChangeEmailDto } from "@/services/auth/dto/requestChangeEmail.dto"
-import { ResetPasswordByTokenDto } from "@/services/auth/dto/resetPasswordByToken.dto"
-import { SignInDto } from "@/services/auth/dto/signIn.dto"
-import { SignUpDto } from "@/services/auth/dto/signUp.dto"
 import { AuthGuard } from "@/services/auth/guards/auth.guard"
 import { AuthProviderGuard } from "@/services/auth/guards/provider.guard"
 import { ProvidersService } from "@/services/auth/providers/providers.service"
-import { OptionalUserDto, UserDto } from "@/services/users/dto/user.dto"
-import { User } from "@/services/users/user.decorator"
-import { AlreadyAuthenticatedError, NoCodeProvidedError, SessionError } from "@/shared/errors/auth"
-import { getMillisecondsFromHours } from "@/shared/utils/getMillisecondsFromHours"
-import { getMillisecondsFromMins } from "@/shared/utils/getMillisecondsFromMins"
+import { NoCodeProvidedError, SessionError } from "@/shared/errors/auth"
 import { getUserWithoutPassword } from "@/shared/utils/getUserWithoutPassword"
-import {
-  AuthControllerDocs,
-  ConfirmChangeEmailDocs,
-  ConfirmEmailDocs,
-  LogoutDocs,
-  OAuthCallbackDocs,
-  OAuthConnectDocs,
-  RecoverPasswordDocs,
-  RequestChangeEmailDocs,
-  ResetPasswordDocs,
-  SendConfirmEmailDocs,
-  SignInDocs,
-  SignUpDocs,
-} from "./auth.controller.docs"
+import { AuthControllerDocs, LogoutDocs, OAuthCallbackDocs, OAuthConnectDocs } from "./auth.controller.docs"
 
 @AuthControllerDocs()
 @Controller("auth")
@@ -58,101 +33,101 @@ export class AuthController {
   ) {
   }
 
-  @Post("/sign-up")
-  @SignUpDocs()
-  async signUp(@Req() req: Request, @Body() body: SignUpDto) {
-    const user = await this.authService.signUp(body)
+  // @Post("/sign-up")
+  // @SignUpDocs()
+  // async signUp(@Req() req: Request, @Body() body: SignUpDto) {
+  //   const user = await this.authService.signUp(body)
+  //
+  //   req.session.user = getUserWithoutPassword(user)
+  //   await this.saveSession(req)
+  // }
+  //
+  // @Post("/sign-in")
+  // @SignInDocs()
+  // async signIn(@Req() req: Request, @Body() body: SignInDto, @User() currentUser: OptionalUserDto) {
+  //   if (currentUser) {
+  //     throw new AlreadyAuthenticatedError()
+  //   }
+  //
+  //   const user = await this.authService.signIn(body)
+  //
+  //   req.session.user = getUserWithoutPassword(user)
+  //   await this.saveSession(req)
+  // }
 
-    req.session.user = getUserWithoutPassword(user)
-    await this.saveSession(req)
-  }
-
-  @Post("/sign-in")
-  @SignInDocs()
-  async signIn(@Req() req: Request, @Body() body: SignInDto, @User() currentUser: OptionalUserDto) {
-    if (currentUser) {
-      throw new AlreadyAuthenticatedError()
-    }
-
-    const user = await this.authService.signIn(body)
-
-    req.session.user = getUserWithoutPassword(user)
-    await this.saveSession(req)
-  }
-
-  @Throttle({
-    default: {
-      limit: 3,
-      ttl: getMillisecondsFromMins(30),
-    },
-  })
-  @Post("/recover-password")
-  @RecoverPasswordDocs()
-  async getRecoverPasswordEmail(@Body() body: GetRecoverPasswordEmailDto) {
-    return this.authService.sendPasswordRecoveryEmail(body.email)
-  }
-
-  @Post("/reset-password")
-  @ResetPasswordDocs()
-  async resetPasswordByToken(
-    @Req() req: Request,
-    @Body() body: ResetPasswordByTokenDto,
-  ) {
-    const user = await this.authService.resetPasswordByToken(body.token, body.password)
-
-    req.session.user = getUserWithoutPassword(user)
-    await this.saveSession(req)
-  }
-
-  @Throttle({
-    default: {
-      limit: 3,
-      ttl: getMillisecondsFromHours(12),
-    },
-  })
-  @UseGuards(AuthGuard)
-  @Post("/change-email")
-  @RequestChangeEmailDocs()
-  async requestChangeEmail(@User() user: UserDto, @Body() body: RequestChangeEmailDto) {
-    return this.authService.requestChangeEmail(user.id, body.email)
-  }
-
-  @Patch("/change-email")
-  @ConfirmChangeEmailDocs()
-  @UseGuards(AuthGuard)
-  async confirmEmailChanging(@Req() req: Request, @User() user: UserDto, @Body() body: ConfirmChangeEmailDto) {
-    const updatedUser = await this.authService.confirmEmailChanging(body.token)
-
-    req.session.user = getUserWithoutPassword(updatedUser)
-    await this.saveSession(req)
-  }
-
-  @Throttle({
-    default: {
-      limit: 1,
-      ttl: getMillisecondsFromMins(15),
-    },
-  })
-  @Get("/confirm-email")
-  @SendConfirmEmailDocs()
-  @UseGuards(AuthGuard)
-  async getConfirmEmail(@User() user: UserDto) {
-    return this.authService.sendConfirmationEmail(user.email)
-  }
-
-  @Patch("/confirm-email")
-  @ConfirmEmailDocs()
-  @UseGuards(AuthGuard)
-  async confirmEmail(
-    @Req() req: Request,
-    @User() user: UserDto,
-    @Body() body: ConfirmEmailDto,
-  ) {
-    await this.authService.confirmEmail(user.email, body.token)
-
-    req.session.user.isEmailVerified = true
-    await this.saveSession(req)
-  }
+  // @Throttle({
+  //   default: {
+  //     limit: 3,
+  //     ttl: getMillisecondsFromMins(30),
+  //   },
+  // })
+  // @Post("/recover-password")
+  // @RecoverPasswordDocs()
+  // async getRecoverPasswordEmail(@Body() body: GetRecoverPasswordEmailDto) {
+  //   return this.authService.sendPasswordRecoveryEmail(body.email)
+  // }
+  //
+  // @Post("/reset-password")
+  // @ResetPasswordDocs()
+  // async resetPasswordByToken(
+  //   @Req() req: Request,
+  //   @Body() body: ResetPasswordByTokenDto,
+  // ) {
+  //   const user = await this.authService.resetPasswordByToken(body.token, body.password)
+  //
+  //   req.session.user = getUserWithoutPassword(user)
+  //   await this.saveSession(req)
+  // }
+  //
+  // @Throttle({
+  //   default: {
+  //     limit: 3,
+  //     ttl: getMillisecondsFromHours(12),
+  //   },
+  // })
+  // @UseGuards(AuthGuard)
+  // @Post("/change-email")
+  // @RequestChangeEmailDocs()
+  // async requestChangeEmail(@User() user: UserDto, @Body() body: RequestChangeEmailDto) {
+  //   return this.authService.requestChangeEmail(user.id, body.email)
+  // }
+  //
+  // @Patch("/change-email")
+  // @ConfirmChangeEmailDocs()
+  // @UseGuards(AuthGuard)
+  // async confirmEmailChanging(@Req() req: Request, @User() user: UserDto, @Body() body: ConfirmChangeEmailDto) {
+  //   const updatedUser = await this.authService.confirmEmailChanging(body.token)
+  //
+  //   req.session.user = getUserWithoutPassword(updatedUser)
+  //   await this.saveSession(req)
+  // }
+  //
+  // @Throttle({
+  //   default: {
+  //     limit: 1,
+  //     ttl: getMillisecondsFromMins(15),
+  //   },
+  // })
+  // @Get("/confirm-email")
+  // @SendConfirmEmailDocs()
+  // @UseGuards(AuthGuard)
+  // async getConfirmEmail(@User() user: UserDto) {
+  //   return this.authService.sendConfirmationEmail(user.email)
+  // }
+  //
+  // @Patch("/confirm-email")
+  // @ConfirmEmailDocs()
+  // @UseGuards(AuthGuard)
+  // async confirmEmail(
+  //   @Req() req: Request,
+  //   @User() user: UserDto,
+  //   @Body() body: ConfirmEmailDto,
+  // ) {
+  //   await this.authService.confirmEmail(user.email, body.token)
+  //
+  //   req.session.user.isEmailVerified = true
+  //   await this.saveSession(req)
+  // }
 
   @Get("/oauth/callback/:provider")
   @OAuthCallbackDocs()
