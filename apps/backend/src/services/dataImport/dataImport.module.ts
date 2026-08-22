09fda90eb75344cee6302af14346254e74854942
dapzer <danilavoronkov2002@gmail.com>
2026-08-22 18:45:38 +0300

    feat(dataImport): add media item repository to data import module

--- apps/backend/src/services/dataImport/dataImport.module.ts
+++ apps/backend/src/services/dataImport/dataImport.module.ts
@@ -1,6 +1,8 @@
 import { Module } from "@nestjs/common"
 import { DataImportRepositorySymbol } from "@/repositories/dataImport/DataImportRepositoryInterface"
 import { DrizzleDataImportRepository } from "@/repositories/dataImport/DrizzleDataImportRepository"
+import { DrizzleMediaItemRepository } from "@/repositories/mediaItem/DrizzleMediaItemRepository"
+import { MediaItemRepositorySymbol } from "@/repositories/mediaItem/MediaItemRepositoryInterface"
 import { DataImportService } from "@/services/dataImport/dataImport.service"
 import { DataImportProvidersModule } from "@/services/dataImport/providers/providers.module"
 import { TmdbResolver } from "@/services/dataImport/providers/services/base/tmdbResolver"
@@ -24,6 +26,7 @@ import { MediaListsServiceModule } from "@/services/mediaLists/mediaLists.module
   providers: [
     DataImportService,
     { provide: DataImportRepositorySymbol, useClass: DrizzleDataImportRepository },
+    { provide: MediaItemRepositorySymbol, useClass: DrizzleMediaItemRepository },
   ],
   exports: [DataImportService],
 })
