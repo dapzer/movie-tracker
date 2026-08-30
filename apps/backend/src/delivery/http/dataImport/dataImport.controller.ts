3a271af52cbcfac1f6153ceda207dacb65920708
dapzer <danilavoronkov2002@gmail.com>
2026-08-30 18:46:21 +0300

    feat(dataImport): implement throttling for data import and processing endpoints

--- apps/backend/src/delivery/http/dataImport/dataImport.controller.ts
+++ apps/backend/src/delivery/http/dataImport/dataImport.controller.ts
@@ -1,7 +1,8 @@
 import { Buffer } from "node:buffer"
-import { getBytesFromMegabytes } from "@movie-tracker/utils"
+import { getBytesFromMegabytes, getMillisecondsFromMins } from "@movie-tracker/utils"
 import { Body, Controller, Get, Param, Post, Query, UploadedFile, UseGuards, UseInterceptors } from "@nestjs/common"
 import { FileInterceptor } from "@nestjs/platform-express"
+import { Throttle } from "@nestjs/throttler"
 import {
   DataImportControllerDocs,
   GetDataImportByIdDocs,
@@ -9,6 +10,7 @@ import {
   ImportDataDocs,
   ProcessDataImportDocs,
 } from "@/delivery/http/dataImport/dataImport.controller.docs"
+import { ThrottlerBehindProxyGuard } from "@/guards/throttlerBehindProxy.guard"
 import { AuthGuard } from "@/services/auth/guards/auth.guard"
 import { DataImportService } from "@/services/dataImport/dataImport.service"
 import { ImportDataQueryDto } from "@/services/dataImport/dto/importDataQuery.dto"
@@ -28,7 +30,13 @@ export class DataImportController {
 
   @Post()
   @ImportDataDocs()
-  @UseGuards(AuthGuard)
+  @Throttle({
+    default: {
+      limit: 3,
+      ttl: getMillisecondsFromMins(15),
+    },
+  })
+  @UseGuards(AuthGuard, ThrottlerBehindProxyGuard)
   @UseInterceptors(FileInterceptor("archive", { limits: { fileSize: IMPORT_ARCHIVE_MAX_SIZE_BYTES } }))
   async importData(
     @Query() query: ImportDataQueryDto,
@@ -72,7 +80,13 @@ export class DataImportController {
 
   @Post(":id/process")
   @ProcessDataImportDocs()
-  @UseGuards(AuthGuard)
+  @Throttle({
+    default: {
+      limit: 5,
+      ttl: getMillisecondsFromMins(15),
+    },
+  })
+  @UseGuards(AuthGuard, ThrottlerBehindProxyGuard)
   async processDataImport(
     @Param() params: UuidDto,
     @Body() body: ProcessDataImportDto,
